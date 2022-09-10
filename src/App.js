d9361a9653015466b8d02230680684afb14d2c34
charint4 <rafif.ridhoo@gmail.com>
2022-09-10 16:56:45 +0700

    feat: style coming soon page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Video from "./videos/video.mp4";
+import logo from "./images/digitama-logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>WEB SMS COMING SOON</p>
         <a
@@ -15,7 +16,26 @@ function App() {
         >
           Sample web
         </a>
-      </header>
+      </header> */}
+      <video
+        autoPlay
+        muted
+        loop
+        class="vb"
+        src={Video}
+        type="video/mp4"
+      ></video>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="logo" className="logo" />
+        </div>
+        <div className="main">
+          <h1 className="cms">Coming Soon</h1>
+        </div>
+        <div className="main">
+          <h4 className="lorem">Lorem ipsum dolor sit amet</h4>
+        </div>
+      </div>
     </div>
   );
 }
