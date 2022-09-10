1dcda0ee4032c15a576fe61ee635c51cfe965b48
charint4 <rafif.ridhoo@gmail.com>
2022-09-10 16:17:43 +0700

    feat: change bg color

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>WEB SMS COMING SOON</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://websms-two.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sample web
         </a>
       </header>
     </div>
